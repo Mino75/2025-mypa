797c282ed055ce26139be2d553266f04fbce162e
Mino Randriamanivo <m.randriamanivo@gmail.com>
2026-02-14 15:48:46 +0100

    Remove service worker registration from main.js
    Removed service worker registration code.

--- main.js
+++ main.js
@@ -168,12 +168,3 @@ function scrollToPreviousScreen() {
     mainContent.scrollTo({ top: lastScreen.offsetTop, behavior: 'smooth' });
   }
 }
-  
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => console.log('Service Worker registered with scope:', registration.scope))
-      .catch(error => console.error('Service Worker registration failed:', error));
-  });
-}
